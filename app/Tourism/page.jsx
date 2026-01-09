e1f852bea1145a0f66cb3de1ef5c4064a6bea836
Shyam Shah Thakuri <thakurishyam111@gmail.com>
2026-01-09 19:41:46 +0545

    project

--- app/Tourism/page.jsx
+++ app/Tourism/page.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Footer from "../component/Footer";
-import Importance from "../component/importance";
-import { Places } from "../component/Places";
-import About from "../component/About";
-import Gallary from "../component/Gallary";
-import Contac from "../component/Contac";
-import Services from "../component/Services";
+import Footer from "../../component/Footer";
+import { Places } from "../../component/Places";
+import About from "../../component/About";
+import Contac from "../../component/Contac";
+import Services from "../../component/Services";
+import Gallary from "../../component/gallary/Gallary";
 
 const App = () => {
-  return (
+  return (<>
+
+
     <div className="font-sans">
       <div className="h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1501785888041-af3ef285b470)" }}>
         <div className="bg-black/60 p-10 rounded-2xl text-center">
@@ -30,6 +31,7 @@ const App = () => {
 
 
     </div>
+    </>
   );
 }
 export default App;
